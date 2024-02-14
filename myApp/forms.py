62fae9e9919052aaa0fa06976e72c8d08aa5c990
i1zzi <gtnsoe88whj@gmail.com>
2024-02-14 22:20:38 +0100

    -added hearts to the page -clean up all the comments -translated few things in eng -translated comments

--- myApp/forms.py
+++ myApp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import CustomUser, Citizen, Slot_Type, Appointment, Booking
 
+# Citizen registration form, also usese a Django User auth. form, but with some modifications via CustomUser
 class CitizenRegistrationForm(forms.ModelForm):
     email = forms.EmailField(required=True)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
@@ -26,6 +27,7 @@ class CitizenRegistrationForm(forms.ModelForm):
             citizen.save()
             return citizen
 
+# Uses basic Django ModelForm, the major info about the appointment
 class SlotForm(forms.ModelForm):
     class Meta:
         model = Appointment
@@ -36,41 +38,34 @@ class SlotForm(forms.ModelForm):
 
         }
             
-# class BookingForm(forms.ModelForm):
-#     class Meta:
-#         model = Booking
-#         fields = []  # Все данные определяются в представлении    
-
-
-## так последняя запись, вообще, оно работает, и даже показывает сообщение о том, что нельзя забронировать.
-        #но, проблема в том, что теперь он показывает все слоты(даже не активные, которые были забронированы другим пользователем)
-        #и до кучи все равно бронирует Collect Passport, после сообщения о том, что его нельзя забронировать.
+# Still has some issuies with double booking. Basically, 'cos of the clean metho. I developed an integreate it after tha major part,
+# so i was not able to fix it in last minute, sorry 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['slot']  # Поле выбора слота для бронирования
+        fields = ['slot']  # Here's the problem, you need to choose the slot again, but you've alread choose it on citizen_profile
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)  # Принимаем пользователя из представления
+        self.user = kwargs.pop('user', None)  # Take user from view
         super().__init__(*args, **kwargs)
 
     def clean(self):
         cleaned_data = super().clean()
         slot = cleaned_data.get("slot")
 
-        # Добавляем проверку на None для объекта slot
+        # cheking if slot is NONO
         if slot is None:
-            raise ValidationError("Необходимо выбрать слот для бронирования.")
+            raise ValidationError("Please, choose a slot")
 
 
-        # Проверка, что пользователь пытается забронировать "Collect passport"
+        # cheking if user tying to book "Collect passport"
         if slot.slot_name.name == "Collect passport":
-            # Проверяем, есть ли у пользователя забронированные слоты на выпуск паспорта
+            # cheking if user has "passport release" befor booking collect
             user_citizen = Citizen.objects.get(user=self.user)
             release_slots = Slot_Type.objects.filter(name__contains="Passport release")
             user_bookings = Booking.objects.filter(user=user_citizen, slot__slot_name__in=release_slots)
         
-        # Тут сортируем по времени слоты у пользователя
+        # Sorting by time, so collect can bee booked only in 30+ days after release
         if slot.slot_name.name == "Collect passport":
             user_citizen = Citizen.objects.get(user=self.user)
             release_slots = Booking.objects.filter(
@@ -78,37 +73,15 @@ class BookingForm(forms.ModelForm):
                 slot__slot_name__name__contains="Passport release"
             ).order_by('-slot__start_time')
             
-        # Проверяем, пооршло ли больше 30 дней с моменты подачи
+        # Cheking if 30+ days
             if release_slots.exists():
                 last_release_date = release_slots.first().slot.start_time
                 if (slot.start_time - last_release_date).days < 30:
-                    raise ValidationError("Сбор паспорта доступен не ранее чем через 30 дней после запроса на выпуск.")
+                    raise ValidationError("Passport collection is available no earlier than 30 days after the request for release.")
 
             if not user_bookings.exists():
-                raise ValidationError("Вы не можете забронировать 'Collect passport', не имея забронированных слотов на 'Passport release'")
+                raise ValidationError("You cannot book 'Collect passport' without having booked slots on 'Passport release'")
 
         return cleaned_data
 
-# на потом
-# class BookingForm(forms.ModelForm):
-#     class Meta:
-#         model = Booking
-#         fields = ['slot']  # Пользователь выбирает слот при бронировании
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         slot = cleaned_data.get('slot')
-        
-#         # Проверка на конкретные условия зависимости
-#         if slot and slot.slot_name.name == "Collect passport":
-#             # Проверяем, есть ли у пользователя необходимые бронирования
-#             # Это может потребовать выполнения запросов к базе данных
-#             # Например, проверить, есть ли у текущего пользователя бронирование типа "Passport release"
-#             has_required_booking = ...  # Здесь должна быть ваша логика проверки
-
-#             if not has_required_booking:
-#                 # Если условие не удовлетворено, добавляем ошибку к форме
-#                 self.add_error('slot', "Сначала необходимо забронировать слот 'Passport release'.")
-
-#         return cleaned_data
 
--- myApp/views.py
+++ myApp/views.py
@@ -7,12 +7,12 @@ from .models import CustomUser, Slot_Type, Appointment, Booking
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 
-# Иморт для букинда аппойтментов
+# for appointment's booking
 from django.views.generic import CreateView, ListView, UpdateView
 from django.urls import reverse_lazy
 
-
-# Формы, отвечающие за регистрацию и вход пользователя
+# CITIZEN FORMS
+# home, registration and login forms
 def home(request):
     return render(request, 'home.html')
 
@@ -50,49 +50,50 @@ def citizen_register(request):
 # тут то же, но с попыткой показывать заброненые слоты, и оно даже работает
 @login_required
 def citizen_profile(request):
-    # Получаем текущего пользователя
+    # getting the user
     user = request.user
-    # Предполагаем, что у пользователя есть связь с моделью Citizen через OneToOneField
-    # и что модель Booking связана с Citizen
+    # Assume that the user has a connection to the Citizen model via OneToOneField
+     # and that the Booking model is related to Citizen
     citizen = user.citizen
-    # Получаем все забронированные слоты для текущего пользователя
+    # Get all reserved slots for the current user
     booked_slots = Booking.objects.filter(user=citizen)
-    # Получаем все доступные для бронирования слоты
+    # get all slots available for booking
     avaliable_slots = Appointment.objects.filter(is_avaliable=True)
     
-    # Передаем забронированные и доступные слоты в шаблон
+    # Pass reserved and available slots to the template
     context = {
         'booked_slots': booked_slots,
         'avaliable_slots': avaliable_slots,
     }
     return render(request, 'citizen_profile.html', context)
 
-# Начальная страница для ментов
+#POLICE FORMS
+# police stuff home-page
 def manage_slots(request):
     return render(request, 'manage_slots.html') # тут пока не понятно, нужна ли эта форма
 
-#создание нового свободного слота ментом
+#create a new slot by police
 class SlotCreateView(CreateView):
     model = Appointment
     form_class = SlotForm
     template_name = 'slot_create.html' # пока нет тэмплэйта
     success_url = reverse_lazy('slot_list')  # пока нет URL с именем 'slot_list'
 
-#просмотр всех созданных слотов ментами
+#view all the slots by police
 class SlotListView(ListView):
     model = Appointment
     context_object_name = 'slot_list'
     template_name = 'slot_list.html'
 
-#в теории, редакирование слота, но хуй оно мне надо, если эёот можно делать через Django admin
+#в теории, редакирование слота, но хуй оно мне надо, если это можно делать через Django admin
 class SlotUpdateView(UpdateView):
     model = Appointment
     form_class = SlotForm
     template_name = 'slots/slot_form.html'
     success_url = reverse_lazy('slot_list')
 
-#вот это букинг аппойтмента со свтороны пользователя, помни о том, как написано is_avaliable
-#вот тут начали менять 
+##this is the booking of the application from the user’s side, remember how "is_avaliable" is written
+    
 @login_required
 def book_slot(request, slot_id):
     slot = get_object_or_404(Appointment, pk=slot_id, is_avaliable=True)
@@ -104,10 +105,10 @@ def book_slot(request, slot_id):
             booking.user = citizen_user
             booking.slot = slot
             booking.save()
-            slot.is_avaliable = False  # Обновляем статус слота
+            slot.is_avaliable = False  # updatin the status
             slot.save()
             messages.success(request, f'Booking for {slot.slot_name.name} from {slot.start_time} to {slot.end_time} in {slot.location} was successful. Do not forget downlisted documents:   ')
-            return redirect('citizen_profile') # пофиксили, все ок
+            return redirect('citizen_profile') 
     else:
         form = BookingForm(user=request.user)
     return render(request, 'book_slot.html', {'form': form, 'slot': slot})
